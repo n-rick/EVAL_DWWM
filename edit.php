927c2524a01bbb17fa886167785938d31abb251b
Enrick Rico <e.ficadiere@gmail.com>
2020-10-30 17:19:22 +0100

    Version 1.1

--- edit.php
+++ edit.php
@@ -1,72 +1,71 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="author" content="Enrick FICADIERE">
-    <title>EDIT </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Enrick FICADIERE">
+        <title>EDIT </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <?php
+        <?php
 
-    @$ancienTitre = $_GET['title'];
-    @$ancienneDescrip = $_GET['description'];
-    @$ancienneDate = $_GET['date'];
+        @$ancienTitre = $_GET['titre'];
+        @$ancienneDescrip = $_GET['description'];
+        @$ancienneDate = $_GET['date'];
 
-    if ($_POST) {
-        try {
-            require_once("db.php");
-            $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $cnx->exec("SET NAMES 'UTF8';");
+        if ($_POST) {
+            try {
+                require_once("db.php");
+                $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $cnx->exec("SET NAMES 'UTF8';");
 
-            $id = $_GET['id'];
-            $title = $_POST['title'];
-            $description = $_POST['description'];
-            $date = $_POST['date'];
+                $id = $_GET['id'];
+                $titre = $_POST['titre'];
+                $description = $_POST['description'];
+                $date = $_POST['date'];
 
-            $resultat = $cnx->prepare('UPDATE posts SET post_title = :title, description = :description, post_at = :date WHERE Id = :id');
-            $resultat->bindParam(':id', $id);
-            $resultat->bindParam(':title', $title);
-            $resultat->bindParam(':description', $description);
-            $resultat->bindParam(':date', $date);
+                $resultat = $cnx->prepare('UPDATE posts SET post_title = :title, description = :description, post_at = :date WHERE Id = :id');
+                $resultat->bindParam(':id', $id);
+                $resultat->bindParam(':title', $titre);
+                $resultat->bindParam(':description', $description);
+                $resultat->bindParam(':date', $date);
 
-            $resultat->execute();
+                $resultat->execute();
 
 
-            header('location:index.php');
-        } catch (Exception $ex) {
-            die('Erreur : ' . $ex->getMessage());
+                header('location:index.php');
+            } catch (Exception $ex) {
+                die('Erreur : ' . $ex->getMessage());
+            }
         }
-    }
 
-    ?>
-    <div class="container">
-        <h2> MODIFIER L'ARTICLE</h2>
-        <div class="row">
-            <form method="post" action="" class="col-12 col-md-6">
-                <button class=" btn btn-success float-right" type="button" name="backbtn"><a class="text-decoration-none text-dark" href="index.php">Retour à la liste</a></button>
-                <div class="form-group">
-                    <label for="titre">Saisir le titre :</label>
-                    <input type="text" required class="form-control" id="titre" name="titre" placeholder="Entrer le titre..." value="<?= $ancienTitre ?>">
-                </div>
-                <div class="form-group">
-                    <label for="description">Saisir la description :</label>
-                    <textarea class="form-control" id="description" name="description" required rows="6" placeholder="Saisir une description..."><?= $ancienneDescrip ?></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="date">Saisir une date :</label>
-                    <input type="date" min="1970-01-01" max='<?php echo date('Y-m-d'); ?>' required class="form-control" id="date" name="date" placeholder="Saisir une date..." value="<?= $ancienneDate ?>">
-                </div>
-                <button class=" btn btn-success" type="submit" name="button">SAUVEGARDER</button>
-            </form>
+        ?>
+        <div class="container">
+            <h2> MODIFIER L'ARTICLE</h2>
+            <div class="row">
+                <form method="post" action="" class="col-12 col-md-6">
+                    <button class=" btn btn-success float-right" type="button" name="backbtn"><a class="text-decoration-none text-dark" href="index.php">Retour à la liste</a></button>
+                    <div class="form-group">
+                        <label for="titre">Saisir le titre :</label>
+                        <input type="text" required class="form-control" id="titre" name="titre" placeholder="Entrer le titre..." value="<?= $ancienTitre ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Saisir la description :</label>
+                        <textarea class="form-control" id="description" name="description" required rows="6" placeholder="Saisir une description..."><?= $ancienneDescrip ?></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="date">Saisir une date :</label>
+                        <input type="date" min="1970-01-01" max='<?php echo date('Y-m-d'); ?>' required class="form-control" id="date" name="date" placeholder="Saisir une date..." value="<?= $ancienneDate ?>">
+                    </div>
+                    <button class=" btn btn-success" type="submit" name="button">SAUVEGARDER</button>
+                </form>
+            </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,93 +1,66 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="author" content="Enrick FICADIERE">
-    <title>EVAL DWWM2 </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Enrick FICADIERE">
+        <title>EVAL DWWM2 </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <!-- PARTIE PHP-->
-    <?php
-    echo "<div><h1 class='d-flex justify-content-center alert alert-dark text-dark'>EVAL DWMM2 - Enrick FICADIERE</h1></div>";
-    try {
-        require_once("db.php");
-        $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $cnx->exec("SET NAMES 'utf8';");
+        <!-- PARTIE PHP-->
+        <?php
+        echo "<div><h1 class='d-flex justify-content-center alert alert-dark text-dark'>EVAL DWMM2 - Enrick FICADIERE</h1></div>";
+        
+        try {
+            require_once("db.php");
+            $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $cnx->exec("SET NAMES 'utf8';");
 
-        $result = $cnx->query('SELECT * FROM posts');
-        $rows = $result->fetchAll();
-        $titre = [];
-        $description = [];
-        $date = [];
-        $id = [];
+            $result = $cnx->query('SELECT * FROM posts');
+            $rows = $result->fetchAll();
+            $titre = [];
+            $description = [];
+            $date = [];
+            $id = [];
 
-        foreach ($rows as $row) {
-            $titre[] = $row[1];
-            $description[] = $row[2];
-            $date[] = $row[3];
-            $id[] = $row[0];
+            foreach ($rows as $row) {
+                $titre[] = $row[1];
+                $description[] = $row[2];
+                $date[] = $row[3];
+                $id[] = $row[0];
+            }
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
         }
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-    echo "<br />";
+        echo "<br />";
 
-    // IMPLEMENTATION DE LA BDD DANS UN TALBEAU
-    echo '<button class=" col-md-1 offset-md-8 btn btn-success" type="submit"><a class="text-decoration-none text-dark" href="add.php"><img src="./crud-icon/add.png" alt="image d\'ajout"> CREATE</a></button>';
-    $table = '<table class="table table-bordered col-12 col-lg-6 mx-auto">';
-    $table .= '<thead>
-        <tr class="table-secondary">
-        <th scope="col"> TITRE </th>
-        <th class="d-flex justify-content-center" width"" scope="col"> DESCRIPTION </th>
-        <th width=13% scope ="col"> DATE </th>
-        <th scope="col"> ACTION </th>
-    </tr>';
-        for ($i = 0; $i < count($rows); $i++) {
-            $table .= '<tr>
-        <th scope="row">' . $titre[$i] . '</th>
-        <td>' . $description[$i] . '</td>
-        <td>' . $date[$i] . '</td>
-        <td class=" d-flex justify-content-center"> <a href="delete.php?id='.$id[$i].'"><img src="./crud-icon/delete.png" alt="image de suppression"></a>
-        <a href="edit.php?id='.$id[$i].'"><img src="crud-icon/edit.png" alt="image d\'édition"></a> </td>
-    </tr>';
-        }
-        $table .= '</thead></table></div>';
-        echo $table;
-    ?>
-</body>
-
-<!--PARTIE HTML -->
-<!-- FORMULAIRE -->
-<!-- <table class="table table-bordered col-12 col-lg-6 mx-auto">
-    <thead>
-        <tr class="table-secondary"> 
+        // IMPLEMENTATION DE LA BDD DANS UN TALBEAU
+        echo '<button class=" col-md-1 offset-md-8 btn btn-success" type="submit"><a class="text-decoration-none text-dark" href="add.php"><img src="./crud-icon/add.png" alt="image d\'ajout"> CREATE</a></button>';
+        $table = '<table class="table table-bordered col-12 col-lg-6 mx-auto">';
+        $table .= '<thead>
+            <tr class="table-secondary">
             <th scope="col"> TITRE </th>
-            <th scope="col"> DESCRIPTION </th>
-            <th scope="col"> DATE </th>
+            <th class="d-flex justify-content-center" width"" scope="col"> DESCRIPTION </th>
+            <th width=13% scope ="col"> DATE </th>
             <th scope="col"> ACTION </th>
-        </tr>
-        <tr>
-            <th scope="row"> </th>
-            <td> </td>
-            <td> </td>
-            <td class=" d-flex justify-content-center"> <a href="delete.php"><img src="crud-icon\delete.png" alt="image de suppression"></a> <a href="edit.php"><img src="crud-icon/edit.png" alt="image d'ajout"></a></td>
-        </tr>
-        <tr>
-            <th scope="row"> </th>
-            <td> </td>
-            <td> </td>
-            <td class="d-flex justify-content-center"> <a href="delete.php"><img src="crud-icon/delete.png" alt="image de suppression"></a> <a href="edit.php"><img src="crud-icon/edit.png" alt="image d'ajout"></a></td>
-        </tr>
-    </thead>
-
-</table> -->
-<!-- FIN DU FORMULAIRE -->
-
+        </tr>';
+            for ($i = 0; $i < count($rows); $i++) {
+                $table .= '<tr>
+            <th scope="row">' . $titre[$i] . '</th>
+            <td>' . $description[$i] . '</td>
+            <td>' . $date[$i] . '</td>
+            <td class=" d-flex justify-content-center"> <a href="delete.php?id='.$id[$i].'"><img src="./crud-icon/delete.png" alt="image de suppression"></a>
+            <a href="edit.php?id='.$id[$i].'"><img src="crud-icon/edit.png" alt="image d\'édition"></a> </td>
+        </tr>';
+            }
+            $table .= '</thead></table></div>';
+            echo $table;
+        ?>
+    </body>
 </html>
